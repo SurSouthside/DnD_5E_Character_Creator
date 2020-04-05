f3db8117f22e417ff72b5bf477be07ac35ef3550
SurSouthside <Stuart.SouthIII1@gmail.com>
2020-04-04 22:23:35 -0400

    Initial Commit

---
+++ app/src/main/java/com/south/DnD_5e_character_creator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.south.DnD_5e_character_creator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
